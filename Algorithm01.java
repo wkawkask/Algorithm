ae96146f08f464944db56e02de6c1c016174228b
김동훈 <wkawkask@naver.com>
2021-09-12 14:31:15 +0900

    java

---
+++ Algorithm01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Algorithm01
+ */
+public class Algorithm01 {
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int money = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println(money);
+
+        
+
+    }
+
+
+
+
+}
