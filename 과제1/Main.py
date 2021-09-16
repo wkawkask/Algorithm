2e0382936e737e77127853970f4c1d805748bdd3
김동훈 <wkawkask@naver.com>
2021-09-16 23:12:59 +0900

    과제 1완료 JAVA & PYTHON

--- 과제1/Main.py
+++ 과제1/Main.py
@@ -1,17 +1,18 @@
 
 n, k = list(map(int, input().split()))
 cnt = 0
-
 array = []
 
 for i in range(0, n):
     array.append(input())
 
-for i in range(0, len(array)):
-    if(int(k) < int(array[i])):
+for i in range(len(array),0,-1):
+    print("sum" , k)
+    if(int(k) < int(array[i-1])):
         continue
-    cnt += int(k)/int(array[i-1])
-    k = int(k) % int(array[i-1])
 
+    print("sum2" , int(k)//int(array[i-1]))
+    cnt += int(k)//int(array[i-1])
+    k = int(k) % int(array[i-1])
 
 print("%d" % cnt)
